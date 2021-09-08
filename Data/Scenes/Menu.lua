41cf92073b405cdd7f1da66bfbe36fe3ff45e75f
Dunnatello <michaeldunn92001@gmail.com>
2021-09-07 21:10:34 -0500

    Added Basic Topbar UI
    Added some initial topbar UI objects to the menu. This could possibly be moved to its own module eventually so that the code isn't repeated in every scene.

--- Data/Scenes/Menu.lua
+++ Data/Scenes/Menu.lua
@@ -23,6 +23,10 @@ local composer = require( "composer" )
 
 local scene = composer.newScene( )
 
+-- Tables
+local ui_Groups = { }
+local ui_Objects = { }
+
 -- -----------------------------------------------------------------------------------
 -- Code outside of the scene event functions below will only be executed ONCE unless
 -- the scene is removed entirely (not recycled) via "composer.removeScene()"
@@ -41,6 +45,55 @@ function scene:create( event )
 	local sceneGroup = self.view
 	-- Code here runs when the scene is first created but has not yet appeared on screen
 
+	-- Create UI Groups
+	for i = 1, 3 do -- Creates a specified amount of UI group layers. Higher numbers are displayed on top of lower groups.
+	
+		ui_Groups[ i ] = display.newGroup( )
+		sceneGroup:insert( ui_Groups[ i ] )
+		
+	end
+	
+	-- Create Topbar
+	local topbarHeight = ( 72 * 2 )
+	ui_Objects[ "Topbar" ] = { }
+	
+	-- Bar
+	ui_Objects[ "Topbar" ].Bar = display.newRect( ui_Groups[ 2 ], display.contentCenterX, ( topbarHeight + display.topStatusBarContentHeight ) / 2, display.contentWidth, topbarHeight + display.topStatusBarContentHeight )
+	ui_Objects[ "Topbar" ].Bar:setFillColor( 76 / 255, 175 / 255, 80 / 255 )
+
+	-- Drop Shadow
+	ui_Objects[ "Topbar" ].DropShadow = display.newRect( ui_Groups[ 1 ], ui_Objects[ "Topbar" ].Bar.x, ui_Objects[ "Topbar" ].Bar.y + ( ui_Objects[ "Topbar" ].Bar.height * 0.025 ), ui_Objects[ "Topbar" ].Bar.width, ui_Objects[ "Topbar" ].Bar.height )
+	ui_Objects[ "Topbar" ].DropShadow:setFillColor( 0, 0, 0, 0.25 )
+	
+	-- Buttons
+	
+	-- Back Button
+	ui_Objects[ "Topbar" ].BackButton = { }
+	local backButton = ui_Objects[ "Topbar" ].BackButton
+	
+	backButton.Button = display.newCircle( ui_Groups[ 2 ], 60 * 1.5, display.topStatusBarContentHeight + 60, 60 )
+	backButton.Button:setFillColor( 8 / 255, 127 / 255, 35 / 255 )
+	
+	backButton.Button.isVisible = false
+
+	backButton.Text = display.newText( { parent = ui_Groups[ 3 ], text = "arrow_back", x = backButton.Button.x, y = backButton.Button.y, font = "Data/Fonts/MaterialIcons-Regular.ttf", fontSize = 72 } )
+
+	-- Settings Button
+	ui_Objects[ "Topbar" ].SettingsButton = { }
+	local settingsButton = ui_Objects[ "Topbar" ].SettingsButton
+	
+	settingsButton.Button = display.newCircle( ui_Groups[ 2 ], display.contentWidth - 60 * 1.5, display.topStatusBarContentHeight + 60, 60 )
+	settingsButton.Button:setFillColor( 8 / 255, 127 / 255, 35 / 255 )
+	
+	settingsButton.Button.isVisible = false
+	
+	settingsButton.Text = display.newText( { parent = ui_Groups[ 3 ], text = "settings", x = settingsButton.Button.x, y = settingsButton.Button.y, font = "Data/Fonts/MaterialIcons-Regular.ttf", fontSize = 72 } )	
+
+	-- Topbar Title
+	ui_Objects[ "Topbar" ].Title = display.newText( { parent = ui_Groups[ 3 ], text = "Burn it Up!", font = "Data/Fonts/Roboto.ttf", fontSize = 72 } )
+	ui_Objects[ "Topbar" ].Title.x = ( backButton.Button.x + backButton.Button.width * 0.6 ) + ui_Objects[ "Topbar" ].Title.width / 2
+	ui_Objects[ "Topbar" ].Title.y = backButton.Button.y
+	
 end
 
 
