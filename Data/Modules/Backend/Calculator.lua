bd9e7f62e0bbccacf6eddb501285b1e501a280e5
Chase <cbalmes45@gmail.com>
2021-10-28 16:51:12 -0500

    calcCaloriesBurned can now be called using exercise string, duration in minutes, and weightLbs

--- Data/Modules/Backend/Calculator.lua
+++ Data/Modules/Backend/Calculator.lua
@@ -2,27 +2,23 @@ local Calculator = {}
 
 metDataModule = require("metData")
 -- Testing a function call from metData.lua
-metDataModule.printMetValues()
+-- metDataModule.printMetValues()
 
-function Calculator.calcCaloriesBurned()
-  -- This will eventually call getMetValue() when user selects an exercise
-  print("Enter MET value: ")
-  METvalue = io.read()
+--This will be needed if we take user input as hours, minutes
+function Calculator.hoursToMinutes(hours, minutes) 
+  minutes = 60 * hours + minutes
+  return minutes
+end
 
-  -- User will input duration in minutes
-  print("Enter duration in minutes: ")
-  duration = io.read()
-
-  -- We either store user weight or have them input it (since it may change often)
-  print("Enter weight in pounds: ")
-  weightKg = io.read() / 2.2
-
-  -- Flooring with + 0.5 rounds to the nearest integer
-  roundedCalories = math.floor((((METvalue * 3.5 * weightKg) / 200 ) * duration) + 0.5)
+function Calculator.calcCaloriesBurned(exercise, minutes, weightLbs)
+  -- Floor + 0.5 rounds to the nearest integer
+  -- Converts lbs input to kg
+  -- Duration needs to be in minutes (subject to change)
+  roundedCalories = math.floor((((metDataModule.getMetValue(exercise) * 3.5 * (weightLbs * 0.453592 )  / 200 ) * minutes) + 0.5))
   print("Calories burned: " .. roundedCalories)
 
 end
 
-calcCaloriesBurned()
+Calculator.calcCaloriesBurned("jump rope", 80, 143.3)
 
 return Calculator
