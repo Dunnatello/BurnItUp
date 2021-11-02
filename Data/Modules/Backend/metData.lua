ce5e5334580368ad648d54d40c1adfc9caeff37b
DifferentFusion <tdb1923@jagmail.southalabama.edu>
2021-11-01 20:17:37 -0500

    Update metData.lua
    no idea why soccer is highlighted red, added all after soccer starting with bowling

--- Data/Modules/Backend/metData.lua
+++ Data/Modules/Backend/metData.lua
@@ -1,13 +1,24 @@
 local metData = {}
 
 metData.metTable = {
+	
 	["bicycling"] = 7.5,   
 	["jump rope"] = 12.3,  
 	["running"] = 8.0,   
 	["walking"] = 3.5,   
 	["tennis"] = 7.3,  
 	["swimming"] = 6.0,   
-	["soccer"] = 7.0    
+	["soccer"] = 7.0,  
+	["bowling"] = 3.0,
+	["boxing"] = 13.4,
+	["basketball"] = 11.1,
+	["car driving"] = 2.0,
+	["cricket"] = 6.1,
+	["croquet"] = 2.5,
+	["curling"] = 7.4,
+	["equestrianism"] = 7.0,
+	["fencing"] = 8.0,
+	["figure skating"] = 8.0
 }
 
 function metData.getMetValue(exercise)
